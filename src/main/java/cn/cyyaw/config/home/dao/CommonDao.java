51c8f1b5afa23721243af062887c0769d7151f7b
why <1663784811@qq.com>
2020-06-14 06:29:45 +0800

    ssss

--- src/main/java/cn/cyyaw/config/home/dao/CommonDao.java
+++ src/main/java/cn/cyyaw/config/home/dao/CommonDao.java
@@ -7,10 +7,24 @@ import java.util.Map;
 
 public interface CommonDao {
 
-
+    /**
+     * 通用查询
+     * @param json
+     * @return
+     */
     Map<String, Object> query(JSONObject json);
 
+    /**
+     * 通用更新
+     * @param json
+     * @return
+     */
     Map<String, Object> update(JSONObject json);
 
+    /**
+     * 通用删除
+     * @param json
+     * @return
+     */
     List<Map<String, Object>> delete(JSONObject json);
 }
