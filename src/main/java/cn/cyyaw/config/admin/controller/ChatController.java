b86238f536953809dce77c03396df98accda1445
why <1663784811@qq.com>
2020-06-21 14:55:52 +0800

    ssss

--- src/main/java/cn/cyyaw/config/admin/controller/ChatController.java
+++ src/main/java/cn/cyyaw/config/admin/controller/ChatController.java
@@ -1,8 +1,5 @@
 package cn.cyyaw.config.admin.controller;
 
-import cn.cyyaw.config.admin.service.ChatService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChatController {
 
-    @Autowired
-    private ChatService chatService;
 
-    /**
-     * 获取用户聊天列表
-     */
-    @GetMapping(value = "/userList")
-    public void userList(){
-        // chatService.userList();
-
-    }
 }
--- src/main/java/cn/cyyaw/config/admin/service/impl/ChatServiceImpl.java
+++ src/main/java/cn/cyyaw/config/admin/service/impl/ChatServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.cyyaw.config.admin.service.impl;
 
 import cn.cyyaw.config.admin.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
